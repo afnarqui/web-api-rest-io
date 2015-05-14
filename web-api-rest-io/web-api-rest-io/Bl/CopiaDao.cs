e06781bef59b1d91621d4a86075cc98d25b8217f
afnarqui <afnarqui@hotmail.com>
2015-05-14 18:00:49 -0500

    Se implementa Dao para la entidad copia
    Se implementa Dao para la entidad copia

---
+++ web-api-rest-io/web-api-rest-io/Bl/CopiaDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api_rest_io.Bl
+{
+    public class CopiaDao
+    {
+        /// <summary>
+        /// andres naranjo
+        /// </summary>
+        /// <returns></returns>
+        public string metodo() {
+
+            return "vacio";
+        }
+    }
+}
